5588029e122fca12977a958a206e132d63132730
marvmilo <marvin.milojevic@gmail.com>
2021-08-11 19:12:18 +0000

    fixed logging bug

--- marvmiloTools/__init__.py
+++ marvmiloTools/__init__.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 import threading
 import time
+import os
 
 #load other scripts
 from . import dash_tools as dash
 from . import json_tools as json
 from . import dictionary_tools as dictionary
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 
 #print command with Script name in front
 class ScriptPrint:
@@ -18,43 +19,79 @@ class ScriptPrint:
         self.block = block
         self.log = log
         self.logfile = "output.log"
+        self.templogfile = f"temp-{self.logfile}"
         self.logrange = dt.timedelta(days = 1)
         self.logtimeformat = "%d_%m_%Y %H:%M:%S.%f"
-        self.__logwriting__ = False
     def print(self, msg):
         if not self.block:
             print(f"[{self.name}]: {msg}")
         if self.log:
-            log_thread = threading.Thread(target = self.__log__thread__, args = (msg, dt.datetime.now(), ))
+            log_thread = threading.Thread(target = self.__log__thread__, args = (msg,))
             log_thread.start()
-    def __log__thread__(self, msg, logtime):
+    def __log__thread__(self, msg):
+        i = 0
+        logtime = dt.datetime.now()
         while True:
-            if not self.__logwriting__:
-                self.__logwriting__ = True
+            files = os.listdir()
+            if not self.templogfile in files and self.logfile in files:
                 try:
-                    with open(self.logfile, "r") as rd:
+                    os.rename(self.logfile, self.templogfile)
+                    with open(self.templogfile, "r") as rd:
                         logmsg = str(msg).replace('\n', ' ')
                         loglines = np.array([l for l in rd.read().split("\n") if not l == ""])
                         del_logs = list()
                         for i,l in enumerate(loglines):
                             try:
-                                logtime = dt.datetime.strptime(l.split(" -> ")[0], self.logtimeformat)
-                                if logtime < logtime - self.logrange:
-                                    print(logtime)
+                                cur_logtime = dt.datetime.strptime(l.split(" -> ")[0], self.logtimeformat)
+                                if cur_logtime < logtime - self.logrange:
                                     del_logs.append(i)
                             except:
                                 del_logs.append(i)
                         loglines = np.delete(loglines, del_logs)
                         loglines = np.append(loglines, f"{logtime.strftime(self.logtimeformat)} -> [{self.name}]: {logmsg}")
-                    with open(self.logfile, "w") as wd:
+                    with open(self.templogfile, "w") as wd:
                         wd.write("\n".join(loglines))
+                    os.rename(self.templogfile, self.logfile)
+                    break
                 except FileNotFoundError:
-                    open(self.logfile, "w")
-                    self.print(msg)
-                self.__logwriting__ = False
-                break
+                    pass
+            elif not self.logfile in files and not self.templogfile in files:
+                open(self.logfile, "w")
+            elif i > 50:
+                try:
+                    os.rename(self.templogfile, self.logfile)
+                except FileNotFoundError:
+                    pass
             else:
-                time.sleep(0.1)
+                time.sleep(0.25)
+                i += 1
+
+
+        # while True:
+        #     if not self.__logwriting__:
+        #         try:
+        #             with open(self.logfile, "r") as rd:
+        #                 logmsg = str(msg).replace('\n', ' ')
+        #                 loglines = np.array([l for l in rd.read().split("\n") if not l == ""])
+        #                 del_logs = list()
+        #                 for i,l in enumerate(loglines):
+        #                     try:
+        #                         logtime = dt.datetime.strptime(l.split(" -> ")[0], self.logtimeformat)
+        #                         if logtime < logtime - self.logrange:
+        #                             print(logtime)
+        #                             del_logs.append(i)
+        #                     except:
+        #                         del_logs.append(i)
+        #                 loglines = np.delete(loglines, del_logs)
+        #                 loglines = np.append(loglines, f"{logtime.strftime(self.logtimeformat)} -> [{self.name}]: {logmsg}")
+        #             with open(self.logfile, "w") as wd:
+        #                 wd.write("\n".join(loglines))
+        #         except FileNotFoundError:
+        #             open(self.logfile, "w")
+        #             self.print(msg)
+        #         break
+        #     else:
+        #         time.sleep(0.1)
             
             
 #Timer for Script runtimes
