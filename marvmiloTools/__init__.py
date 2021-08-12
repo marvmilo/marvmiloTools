7a55a33efdafe39d6ac33d9d4c564bcf39e2f567
marvmilo <marvin.milojevic@gmail.com>
2021-08-12 21:01:12 +0000

    deleted logging function

--- marvmiloTools/__init__.py
+++ marvmiloTools/__init__.py
@@ -10,52 +10,16 @@ from . import dash_tools as dash
 from . import json_tools as json
 from . import dictionary_tools as dictionary
 
-__version__ = "1.5.5"
+__version__ = "1.6.0"
 
 #print command with Script name in front
 class ScriptPrint:
-    def __init__(self, name, block = False, log = False):
+    def __init__(self, name, block = False):
         self.name = name
         self.block = block
-        self.log = log
-        self.logfile = "output.log"
-        self.logrange = dt.timedelta(days = 1)
-        self.logtimeformat = "%d_%m_%Y %H:%M:%S.%f"
-        self.loglist = {}
-        self.running = True
-        self.thread = threading.Thread(target=self.__log_thread__)
-        self.thread.start()
     def print(self, msg):
         if not self.block:
             print(f"[{self.name}]: {msg}")
-        if self.log:
-            self.loglist[dt.datetime.now().strftime(self.logtimeformat)] = msg
-    def finish(self):
-        self.running = False
-    def __log_thread__(self):
-        while True:
-            if self.running:
-                with open(self.logfile, "r") as rd:
-                    loglines = np.array([l for l in rd.read().split("\n") if not l == ""])
-                    del_logs = list()
-                    for i,l in enumerate(loglines):
-                        try:
-                            cur_logtime = dt.datetime.strptime(l.split(" -> ")[0], self.logtimeformat)
-                            if cur_logtime < dt.datetime.now() - self.logrange:
-                                del_logs.append(i)
-                        except:
-                            del_logs.append(i)
-                    loglines = np.delete(loglines, del_logs)
-                    loglines = np.sort(loglines)
-                    for ts, log in self.loglist.items():
-                        log = str(log).replace('\n', ' ')
-                        loglines = np.append(loglines, f"{ts} -> [{self.name}]: {log}")
-                with open(self.logfile, "w") as wd:
-                    wd.write("\n".join(loglines))
-                self.loglist = {}
-                time.sleep(10)
-            else:
-                break  
             
 #Timer for Script runtimes
 class Timer:
