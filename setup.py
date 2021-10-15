2286c58c522621f3a44edff789c87017a2e33937
marvmilo <marvin.milojevic@gmail.com>
2021-10-15 14:36:47 +0200

    added paho-mqtt package

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
   install_requires=[
     "pandas",
     "dash",
-    "dash-bootstrap-components"
+    "dash-bootstrap-components",
+    "paho-mqtt"
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
